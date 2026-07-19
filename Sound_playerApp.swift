3d4027c5c6d30b21a4518b45bcd6bbfde68a9f03
Moon. <hatsemarian@gmail.com>
2026-07-19 18:47:10 +0300

    the start

---
+++ Sound_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_playerApp.swift
+//  Sound player
+//
+//  Created by Moonie on 20.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Sound_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
